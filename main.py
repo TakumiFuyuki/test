b59067b60134dd7547ad7119c71be71f04198a48
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-03 23:32:21 +0900

    a

--- main.py
+++ main.py
@@ -17,28 +17,28 @@ table_name = 'button'
 def index():
     return render_template('submit.html')
 
-@app.route('/submit', methods=['post']) #htmlでpathを設定する必要がある
-def submit():
-    # ボタンが押された時刻を取得します
-    button_time = datetime.now()
+# @app.route('/submit', methods=['post']) #htmlでpathを設定する必要がある
+# def submit():
+#     # ボタンが押された時刻を取得します
+#     button_time = datetime.now()
 
-    # フォームからボタンの種類を取得します
-    button_type = request.form['button_type']
+#     # フォームからボタンの種類を取得します
+#     button_type = request.form['button_type']
 
-    # BigQueryにデータを挿入します
-    insert_data_to_bigquery(button_time, button_type)
-    return '提出が完了しました。'
+#     # BigQueryにデータを挿入します
+#     insert_data_to_bigquery(button_time, button_type)
+#     return '提出が完了しました。'
 
 
-def insert_data_to_bigquery(button_time, button_type):
-    # データをBigQueryのテーブルに挿入するためのクエリを作成します
-    query = f"""
-        INSERT INTO `{dataset_name}.{table_name}` (datetime, type)
-        VALUES ('{button_time}', '{button_type}')
-    """
+# def insert_data_to_bigquery(button_time, button_type):
+#     # データをBigQueryのテーブルに挿入するためのクエリを作成します
+#     query = f"""
+#         INSERT INTO `{dataset_name}.{table_name}` (datetime, type)
+#         VALUES ('{button_time}', '{button_type}')
+#     """
 
-    # クエリを実行します
-    client.query(query)
+#     # クエリを実行します
+#     client.query(query)
 
 if __name__ == '__main__':
     app.run(port=8080)
