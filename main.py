3338aa11b8558a15005ff50003a5c12d199e2a59
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-03 14:05:18 +0900

    ボタン

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # main.py
 
-from flask import Flask # type: ignore
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is a Flask main running on Google main Engine.'
+    return render_template('submit.html')
+
+@app.route('/submit', methods=['post'])
+def submit():
+    return '提出が完了しました。'
 
 if __name__ == '__main__':
     app.run(port=8080)
