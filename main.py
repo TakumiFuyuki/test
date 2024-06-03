6fcf958f4c0284a0f8cc7783c2955337e990efa6
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-03 14:20:04 +0900

    a

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def hello():
     return render_template('submit.html')
 
-# @app.route('/submit', methods=['post'])
-# def submit():
-#     return '提出が完了しました。'
+@app.route('/submit', methods=['post'])
+def submit():
+    return '提出が完了しました。'
 
 if __name__ == '__main__':
     app.run(port=8080)
