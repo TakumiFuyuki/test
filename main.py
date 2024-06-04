8d1404ab3d6dccd4140bc5e23d3916fb77ee6872
Takkun <fuyuki0121ta@gmail.com>
2024-06-04 12:22:09 +0900

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Google Cloud BigQueryのクライアントを初期化します
-#client = bigquery.Client()
+client = bigquery.Client()
 
 # BigQueryのデータセット名とテーブル名を定義します
 dataset_name = 'datasets'
@@ -26,30 +26,30 @@ def submit():
     button_type = request.form['button_type']
 
     # BigQueryにデータを挿入します
-    #insert_data_to_bigquery(button_time, button_type)
+    insert_data_to_bigquery(button_time, button_type)
     return '提出が完了しました。'
 
 
-# def insert_data_to_bigquery(button_time, button_type):
-#     # ボタン時刻をUNIXタイムスタンプに変換
-#     button_time_timestamp = button_time.timestamp()
+def insert_data_to_bigquery(button_time, button_type):
+    # ボタン時刻をUNIXタイムスタンプに変換
+    button_time_timestamp = button_time.timestamp()
 
-#     # クエリの準備
-#     query = """
-#         INSERT IN#TO `{dataset_name}.{table_name}` (datetime, type)
-#         VALUES (@button_time, @button_type)
-#     """.format(dataset_name=dataset_name, table_name=table_name)
+    # クエリの準備
+    query = """
+        INSERT IN#TO `{dataset_name}.{table_name}` (datetime, type)
+        VALUES (@button_time, @button_type)
+    """.format(dataset_name=dataset_name, table_name=table_name)
 
-#     # クエリパラメータの設定
-#     job_config = bigquery.QueryJobConfig(
-#         query_parameters=[
-#             bigquery.ScalarQueryParameter("button_time", "TIMESTAMP", button_time_timestamp),
-#             bigquery.ScalarQueryParameter("button_type", "STRING", button_type),
-#         ]
-#     )
+    # クエリパラメータの設定
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("button_time", "TIMESTAMP", button_time_timestamp),
+            bigquery.ScalarQueryParameter("button_type", "STRING", button_type),
+        ]
+    )
 
-#     # クエリを実行
-#     client.query(query, job_config=job_config)
+    # クエリを実行
+    client.query(query, job_config=job_config)
 
 if __name__ == '__main__':
     app.run(port=8080)
