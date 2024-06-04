edc333635b372bb8af1d7666ba090b714aed0cf6
TorDataScientist <45032597+TorDataScientist@users.noreply.github.com>
2024-06-04 20:19:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# main.py
-
 from flask import Flask, render_template, request
-from google.cloud import bigquery #install
+from google.cloud import bigquery
 from datetime import datetime
 
 app = Flask(__name__)
@@ -10,46 +8,39 @@ app = Flask(__name__)
 client = bigquery.Client()
 
 # BigQueryのデータセット名とテーブル名を定義します
-dataset_name = 'datasets'
+dataset_name = 'gifted-fragment-425209-s7.datasets'
 table_name = 'button'
 
 @app.route('/')
 def index():
     return render_template('submit.html')
 
-@app.route('/submit', methods=['post']) #htmlでpathを設定する必要がある
+@app.route('/submit', methods=['POST'])
 def submit():
-    # # ボタンが押された時刻を取得します
+    # ボタンが押された時刻を取得します
     button_time = datetime.now()
 
     # フォームからボタンの種類を取得します
     button_type = request.form['button_type']
 
-    # # BigQueryにデータを挿入します
+    # BigQueryにデータを挿入します
     insert_data_to_bigquery(button_time, button_type)
     return '提出が完了しました。'
 
-
 def insert_data_to_bigquery(button_time, button_type):
-    # ボタン時刻をUNIXタイムスタンプに変換
-    button_time_timestamp = button_time.timestamp()
+    # BigQueryに挿入する行のデータを準備
+    rows_to_insert = [
+        {
+            "datetime": button_time,
+            "type": button_type
+        }
+    ]
 
-    # クエリの準備
-    query = """
-        INSERT INTO `{dataset_name}.{table_name}` (datetime, type)
-        VALUES (@button_time, @button_type)
-    """.format(dataset_name=dataset_name, table_name=table_name)
-
-    # クエリパラメータの設定
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("button_time", "TIMESTAMP", button_time_timestamp),
-            bigquery.ScalarQueryParameter("button_type", "STRING", button_type),
-        ]
-    )
-
-    # クエリを実行
-    client.query(query, job_config=job_config)
+    # BigQueryのテーブルにデータを挿入
+    table_id = f"{dataset_name}.{table_name}"
+    errors = client.insert_rows_json(table_id, rows_to_insert)
+    if errors:
+        raise Exception(f"BigQueryへのデータ挿入中にエラーが発生しました: {errors}")
 
 if __name__ == '__main__':
     app.run(port=8080)
