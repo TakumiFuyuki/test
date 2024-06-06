2399349ce16efc6ef7c7e741c5581ff508c28410
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-06 12:08:33 +0900

    いけるかにゃ？

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def submit():
     # BigQueryから最新10件のテキストデータを取得します
     latest_text_data = get_latest_text_data()
     # return 'エラー確認'
-    return render_template('submit.html', latest_text_data=latest_text_data)
+    return render_template('result.html', latest_text_data=latest_text_data) #result.htmlで使用している変数の定義も忘れずに！
 
 def insert_button_data_to_bigquery(button_time, button_type):
     # datetimeオブジェクトをISOフォーマットの文字列に変換
